f14493b082992c80880207b6a19b05a86240e5b1
Philippe Blouin-Leclerc <Phil.19013@gmail.com>
2023-09-19 14:58:28 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,4 +7,11 @@ setup(
     url="https://github.com/philbl/satellite-image-handler",
     author="Philippe Blouin-Leclerc",
     author_email="phil.19013@gmail.com",
+    install_requires=[
+        "geopandas==0.12.2",
+        "numpy==1.26.0",
+        "pandas==2.1.0",
+        "rasterio==1.3.6",
+        "scikit-image==0.20.0",
+    ]
 )
